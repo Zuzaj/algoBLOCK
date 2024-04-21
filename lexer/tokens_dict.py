4eae7e279485e690026c0f943f251cce8507a2ee
Zuzia <zuzia.jarl@gmail.com>
2024-04-21 10:41:25 +0200

    added possible tokens dict

---
+++ lexer/tokens_dict.py
@@ -0,0 +1,6 @@
+tokens_dict = {
+    'Keyword': ['if', 'then', 'while', 'for', 'to', 'do', 'return', 'length', 'floor', 'min', 'max', 'mid','function','and', 'or'],
+    'Var': ['[a-zA-Z_]+'],
+    'Operator': ['=', '+', '-', '*', '/', '<', '>', '(', ')', '[', ']', "?=", "/=", ','],
+    'Literal': ['[0-9]+', '[0-9]+\.[0-9]+']
+}
