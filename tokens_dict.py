4fb5ae50a5b3c226aeacd817a40b018f981298f2
Zuzia <zuzia.jarl@gmail.com>
2024-04-21 10:41:56 +0200

    changed structure

--- tokens_dict.py
+++
@@ -1,6 +0,0 @@
-tokens_dict = {
-    'Keyword': ['if', 'then', 'while', 'for', 'to', 'do', 'return', 'length', 'floor', 'min', 'max', 'mid','function','and', 'or'],
-    'Var': ['[a-zA-Z_]+'],
-    'Operator': ['=', '+', '-', '*', '/', '<', '>', '(', ')', '[', ']', "?=", "/=", ','],
-    'Literal': ['[0-9]+', '[0-9]+\.[0-9]+']
-}
